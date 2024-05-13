cd32538e1bdac49a0f2c12d68eca88cf3bd8df91
Jonathan <minkhant5771@gmail.com>
2024-05-13 16:34:49 +0630

    basic posthog analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,7 @@ import { NextSSRPlugin } from "@uploadthing/react/next-ssr-plugin";
 import { extractRouterConfig } from "uploadthing/server";
 import { ourFileRouter } from "./api/uploadthing/core";
 import { Toaster } from "sonner";
+import { CSPostHogProvider } from "./_analytics/provider";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -29,28 +30,30 @@ export default function RootLayout({
 }) {
   return (
     <ClerkProvider>
-      <html lang="en">
-        <NextSSRPlugin
-          /**
-           * The `extractRouterConfig` will extract **only** the route configs
-           * from the router to prevent additional information from being
-           * leaked to the client. The data passed to the client is the same
-           * as if you were to fetch `/api/uploadthing` directly.
-           */
-          routerConfig={extractRouterConfig(ourFileRouter)}
-        />
-        <body className={`dark font-sans ${inter.variable} `}>
-          <div className="grid h-screen  grid-rows-[auto,1fr]">
-            <TopNav />
-            <main className="overflow-x-hidden overflow-y-scroll">
-              {children}
-            </main>
-          </div>
-          <Toaster />
-          {modal}
-          <div id="modal-root"></div>
-        </body>
-      </html>
+      <CSPostHogProvider>
+        <html lang="en">
+          <NextSSRPlugin
+            /**
+             * The `extractRouterConfig` will extract **only** the route configs
+             * from the router to prevent additional information from being
+             * leaked to the client. The data passed to the client is the same
+             * as if you were to fetch `/api/uploadthing` directly.
+             */
+            routerConfig={extractRouterConfig(ourFileRouter)}
+          />
+          <body className={`dark font-sans ${inter.variable} `}>
+            <div className="grid h-screen  grid-rows-[auto,1fr]">
+              <TopNav />
+              <main className="overflow-x-hidden overflow-y-scroll">
+                {children}
+              </main>
+            </div>
+            <Toaster />
+            {modal}
+            <div id="modal-root"></div>
+          </body>
+        </html>
+      </CSPostHogProvider>
     </ClerkProvider>
   );
 }
